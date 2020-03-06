c570beb0f852ce36912cdbf4922e68a3e54196bf
xia0m <alexma325@gmail.com>
2020-03-06 13:30:05 -0800

    added all the config file for deployment

--- auth.py
+++ auth.py
@@ -3,12 +3,12 @@ from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import os
 
 
-AUTH0_DOMAIN = 'fsnd-auth.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'cast-api'
-
+AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
+ALGORITHMS = os.environ['ALGORITHMS']
+API_AUDIENCE = os.environ['API_AUDIENCE']
 # AuthError Exception
 '''
 AuthError Exception
--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-
+export AUTH0_DOMAIN="fsnd-auth.auth0.com";
+export ALGORITHMS="['RS256']";
+export API_AUDIENCE="cast-api";
+export DATABASE_URL="postgresql://localhost:5432/cast_test";
+export AUTH0_CLIENT_ID="4P1aExVFOpMCb8eAmxfJ6juhDt8a5dcZ";
